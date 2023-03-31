92b1cc425a69ee451a7ea06bb76b953a1dc24a3f
AlanCoutinhoD <106571970+AlanCoutinhoD@users.noreply.github.com>
2023-03-31 08:55:23 -0600

    Update BasededatosApplication.java

--- src/main/java/com/basedatos/basededatos/BasededatosApplication.java
+++ src/main/java/com/basedatos/basededatos/BasededatosApplication.java
@@ -2,7 +2,10 @@ package com.basedatos.basededatos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.basedatos.basededatos.dao.UserDao"})
@@ -15,8 +18,8 @@ public class BasededatosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasededatosApplication.class, args);
-		
-		@Bean
+	}
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
@@ -25,6 +28,4 @@ public class BasededatosApplication {
 			}
 		};
 	}
-	}
-
 }
